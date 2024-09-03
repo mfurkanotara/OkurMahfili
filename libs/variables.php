1f110011a7f5b2bdce4b28b68a1729924b3af75d
Muhammet Furkan Otara <mfurkanotara0@gmail.com>
2024-09-04 01:18:54 +0300

    variables.php düzenlendi.

--- libs/variables.php
+++ libs/variables.php
@@ -1,3 +1,5 @@
+<?php
+
 $oneCikanKitaplar = array(
     "1" => array(
         "kitapAdi" => "Ahirete Yolculuk",
@@ -31,3 +33,61 @@ $oneCikanKitaplar = array(
     ),
 
 )
+
+$tarih = array(
+    "1" => array(
+        "resim" => "../photos/books/tarih/avrupa-ve-biz.jpg",
+        "kitapAdi" => "Avrupa ve Biz",
+        "kitapYazari" => "İlber Ortaylı",
+        "kitapFiyati" => "₺52,19",
+    ),
+    "2" => array(
+        "resim" => "../photos/books/tarih/cumhuriyetin-dogusu.jpg",
+        "kitapAdi" => "Cumhuriyetin Doğuşu",
+        "kitapYazari" => "İlber Ortaylı",
+        "kitapFiyati" => "₺57,10",
+    ),
+    "3" => array(
+        "resim" => "../photos/books/tarih/gazi-mustafa-kemal-atatürk.jpg",
+        "kitapAdi" => "Gazi Mustafa Kemal Atatürk",
+        "kitapYazari" => "İlber Ortaylı",
+        "kitapFiyati" => "₺52,19",
+    ),
+    "4" => array(
+        "resim" => "../photos/books/tarih/gel-dunyayi-kesfedelim.jpg",
+        "kitapAdi" => "Gel Dünyayı Keşfedelim",
+        "kitapYazari" => "İlber Ortaylı",
+        "kitapFiyati" => "₺51,56",
+    ),
+    "5" => array(
+        "resim" => "../photos/books/tarih/islam-ve-bati.jpg",
+        "kitapAdi" => "İslam ve Batı",
+        "kitapYazari" => "İbrahim Kalın",
+        "kitapFiyati" => "₺71,10",
+    ),
+    "6" => array(
+        "resim" => "../photos/books/tarih/kisa-osmanli-tarihi.jpg",
+        "kitapAdi" => "Kısa Osmanlı Tarihi",
+        "kitapYazari" => "Halil İnalcık",
+        "kitapFiyati" => "₺58,44",
+    ),
+    "7" => array(
+        "resim" => "../photos/books/tarih/milli-mucadele-tarihi.jpg",
+        "kitapAdi" => "Milli Mücadele Tarihi",
+        "kitapYazari" => "Halil İnalcık",
+        "kitapFiyati" => "₺41,96",
+    ),
+    "8" => array(
+        "resim" => "../photos/books/tarih/neden-islamin-ortacagi-yoktu.jpg",
+        "kitapAdi" => "Neden İslam'ın Ortaçağı Yoktu?",
+        "kitapYazari" => "Thomas Bauer",
+        "kitapFiyati" => "₺44,00",
+    ),
+    "9" => array(
+        "resim" => "../photos/books/tarih/teskilati-mahsusa.jpg",
+        "kitapAdi" => "Teşkilat-ı Mahsusa",
+        "kitapYazari" => "Philip H. Stoddard",
+        "kitapFiyati" => "₺58,44",
+    ),
+
+?>

d5270a62c629a2f68d2f12ed4cc0fdff0082dba6
Muhammet Furkan Otara <mfurkanotara0@gmail.com>
2024-09-04 00:41:07 +0300

    index.php düzenlendi.

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,11 @@
+<?php
+
+    require "libs/functions.php";
+    require "libs/variables.php";
+
+?>
+
+
 <?php include "../partials/_header.php" ?>
 <?php include "../partials/_navbar.php" ?>
 <?php include "../partials/content/_index-content.php" ?>

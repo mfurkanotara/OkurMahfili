6e37f925a45b391b21ddee63e73fe5cee0b6dbb6
Muhammet Furkan Otara <mfurkanotara0@gmail.com>
2024-09-04 14:48:37 +0300

    _index-content.php dosyası düzenlendi.

--- partials/content/_index-content.php
+++ partials/content/_index-content.php
@@ -1,39 +1,6 @@
 <?php
-
-
-$oneCikanKitaplar = array(
-    "1" => array(
-        "kitapAdi" => "Ahirete Yolculuk",
-        "aciklama" => "Bu açıklama, Ahirete Yolculuk kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/ahirete-yolculuk.jpg",
-    ),
-    "2" => array(
-        "kitapAdi" => "Bir Başka Din Tasavvuf",
-        "aciklama" => "Bu açıklama, Bir Başka Din Tasavvuf kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/bir-baska-din-tasavvuf.jpg",
-    ),
-    "3" => array(
-        "kitapAdi" => "Feyzü'l-Furkan",
-        "aciklama" => "Bu açıklama, Feyzü'l-Furkan kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/feyzul-furkan.jpg",
-    ),
-    "4" => array(
-        "kitapAdi" => "Haber Delili",
-        "aciklama" => "Bu açıklama, Haber Delili kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/haber-delili.jpg",
-    ),
-    "5" => array(
-        "kitapAdi" => "İslam ve Batı",
-        "aciklama" => "Bu açıklama, İslam ve Batı kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/islam-ve-bati.jpg",
-    ),
-    "6" => array(
-        "kitapAdi" => "Muhtelif 1",
-        "aciklama" => "Bu açıklama, Muhtelif 1 kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/muhtelif-1.jpg",
-    ),
-
-)
+    require 'C:\xampp\htdocs\okur-mahfili\libs\variables.php';
+    require 'C:\xampp\htdocs\okur-mahfili\libs\functions.php';
 ?>
 
 

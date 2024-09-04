421c078b1c25ca4ed64d759ec12b0b90b225fcf6
Muhammet Furkan Otara <mfurkanotara0@gmail.com>
2024-09-04 14:34:43 +0300

    variables.php dosyası düzenlendi.

--- libs/variables.php
+++ libs/variables.php
@@ -1,93 +1,289 @@
 <?php
+    $cokSatanlar = array(
+        "1" => array(
+            "resim" => "../photos/books/cok-satanlar/iyi-hissetmek.jpg",
+            "kitapAdi" => "İyi Hissetmek",
+            "kitapYazari" => "Dr. David Burns",
+            "kitapFiyati" => "₺165,03",
+        ),
+        "2" => array(
+            "resim" => "../photos/books/cok-satanlar/ayvayi-yedik-muzesi.jpg",
+            "kitapAdi" => "Ayvayı Yedik Müzesi",
+            "kitapYazari" => "Mert Arık",
+            "kitapFiyati" => "₺52,19",
+        ),
+        "3" => array(
+            "resim" => "../photos/books/cok-satanlar/naneyi-yedik-lokantasi.jpg",
+            "kitapAdi" => "Naneyi Yedik Lokantası",
+            "kitapYazari" => "Mert Arık",
+            "kitapFiyati" => "₺57,10",
+        ),
+        "4" => array(
+            "resim" => "../photos/books/cok-satanlar/cantamdan-fil-cikti.jpg",
+            "kitapAdi" => "Çantamdan Fil Çıktı",
+            "kitapYazari" => "Mert Arık",
+            "kitapFiyati" => "₺52,19",
+        ),
+        "5" => array(
+            "resim" => "../photos/books/cok-satanlar/atomik-aliskanliklar.jpg",
+            "kitapAdi" => "Atomik Alışkanlıklar",
+            "kitapYazari" => "James Clear",
+            "kitapFiyati" => "₺144,30",
+        ),
+        "6" => array(
+            "resim" => "../photos/books/cok-satanlar/yatirim-el-kitabi.png",
+            "kitapAdi" => "Yatırım El Kitabı",
+            "kitapYazari" => "Mehmet Şükrü Tekbaş",
+            "kitapFiyati" => "₺441,00",
+        ),
+        "7" => array(
+            "resim" => "../photos/books/cok-satanlar/salkim-sokak.jpg",
+            "kitapAdi" => "Salkım Sokak No:3",
+            "kitapYazari" => "İclal Aydın",
+            "kitapFiyati" => "₺168,19",
+        ),
+        "8" => array(
+            "resim" => "../photos/books/cok-satanlar/soyleme-bilmesinler.jpg",
+            "kitapAdi" => "Söyleme Bilmesinler",
+            "kitapYazari" => "Şermin Yaşar",
+            "kitapFiyati" => "₺123,20",
+        ),
+        "9" => array(
+            "resim" => "../photos/books/cok-satanlar/dortgun-ucgece.jpg",
+            "kitapAdi" => "4 Gün 3 Gece",
+            "kitapYazari" => "Ayşe Kulin",
+            "kitapFiyati" => "₺102,02",
+        )
 
-$oneCikanKitaplar = array(
-    "1" => array(
-        "kitapAdi" => "Ahirete Yolculuk",
-        "aciklama" => "Bu açıklama, Ahirete Yolculuk kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/ahirete-yolculuk.jpg",
-    ),
-    "2" => array(
-        "kitapAdi" => "Bir Başka Din Tasavvuf",
-        "aciklama" => "Bu açıklama, Bir Başka Din Tasavvuf kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/bir-baska-din-tasavvuf.jpg",
-    ),
-    "3" => array(
-        "kitapAdi" => "Feyzü'l-Furkan",
-        "aciklama" => "Bu açıklama, Feyzü'l-Furkan kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/feyzul-furkan.jpg",
-    ),
-    "4" => array(
-        "kitapAdi" => "Haber Delili",
-        "aciklama" => "Bu açıklama, Haber Delili kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/haber-delili.jpg",
-    ),
-    "5" => array(
-        "kitapAdi" => "İslam ve Batı",
-        "aciklama" => "Bu açıklama, İslam ve Batı kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/islam-ve-bati.jpg",
-    ),
-    "6" => array(
-        "kitapAdi" => "Muhtelif 1",
-        "aciklama" => "Bu açıklama, Muhtelif 1 kitabını açıklıyor.",
-        "resim" => "../photos/books/index-book-img/muhtelif-1.jpg",
-    ),
+        );
+        $edebiyat = array(
+            "1" => array(
+                "resim" => "../photos/books/edebiyat/dortgun-ucgece.jpg",
+                "kitapAdi" => "4 Gün 3 Gece",
+                "kitapYazari" => "Ayşe Kulin",
+                "kitapFiyati" => "₺102,02",
+            ),
+            "2" => array(
+                "resim" => "../photos/books/edebiyat/soyleme-bilmesinler.jpg",
+                "kitapAdi" => "Söyleme Bilmesinler",
+                "kitapYazari" => "Şermin Yaşar",
+                "kitapFiyati" => "₺123,20",
+            ),
+            
+            "3" => array(
+                "resim" => "../photos/books/edebiyat/insanligimi-yitirirken.png",
+                "kitapAdi" => "İnsanlığımı Yitirirken",
+                "kitapYazari" => "Osamu Dazai",
+                "kitapFiyati" => "₺12,88",
+            ),
+            "4" => array(
+                "resim" => "../photos/books/edebiyat/salkim-sokak.jpg",
+                "kitapAdi" => "Salkım Sokak No:3",
+                "kitapYazari" => "İclal Aydın",
+                "kitapFiyati" => "₺168,19",
+            ),
+            "5" => array(
+                "resim" => "../photos/books/edebiyat/kaplanin-sirtinda.jpg",
+                "kitapAdi" => "Kaplanın Sırtında",
+                "kitapYazari" => "Zülfü Livaneli",
+                "kitapFiyati" => "₺108,90",
+            ),
+            "6" => array(
+                "resim" => "../photos/books/edebiyat/saatleri-ayarlama-enstitusu.jpg",
+                "kitapAdi" => "Saatleri Ayarlama Enstitüsü",
+                "kitapYazari" => "Ahmet Hamdi Tanpınar",
+                "kitapFiyati" => "₺240,00",
+            ),
+            "7" => array(
+                "resim" => "../photos/books/edebiyat/labirent.jpg",
+                "kitapAdi" => "Labirent",
+                "kitapYazari" => "Amin Maalouf",
+                "kitapFiyati" => "₺103,41",
+            ),
+            "8" => array(
+                "resim" => "../photos/books/edebiyat/nietzsche-agladiginda.jpg",
+                "kitapAdi" => "Nietzsche Ağladığında",
+                "kitapYazari" => "Irvin D. Yalom",
+                "kitapFiyati" => "₺178,20",
+            ),
+            "9" => array(
+                "resim" => "../photos/books/edebiyat/yasamak.jpg",
+                "kitapAdi" => "Yaşamak",
+                "kitapYazari" => "Yu Hua",
+                "kitapFiyati" => "₺109,38",
+            )
+    
+            );
+            $hikaye = array(
+                "1" => array(
+                    "resim" => "../photos/books/hikaye/ayvayi-yedik-muzesi.jpg",
+                    "kitapAdi" => "Ayvayı Yedik Müzesi",
+                    "kitapYazari" => "Mert Arık",
+                    "kitapFiyati" => "₺52,19",
+                ),
+                "2" => array(
+                    "resim" => "../photos/books/hikaye/naneyi-yedik-lokantasi.jpg",
+                    "kitapAdi" => "Naneyi Yedik Lokantası",
+                    "kitapYazari" => "Mert Arık",
+                    "kitapFiyati" => "₺57,10",
+                ),
+                "3" => array(
+                    "resim" => "../photos/books/hikaye/cantamdan-fil-cikti.jpg",
+                    "kitapAdi" => "Çantamdan Fil Çıktı",
+                    "kitapYazari" => "Mert Arık",
+                    "kitapFiyati" => "₺52,19",
+                ),
+                "4" => array(
+                    "resim" => "../photos/books/hikaye/bu-kadar-tantana-yeter.jpg",
+                    "kitapAdi" => "Bu Kadar Tantana Yeter",
+                    "kitapYazari" => "Mert Arık",
+                    "kitapFiyati" => "₺51,56",
+                ),
+                "5" => array(
+                    "resim" => "../photos/books/hikaye/carpi-iki.jpg",
+                    "kitapAdi" => "Çarpı 2",
+                    "kitapYazari" => "Hatice Kübra Tongar",
+                    "kitapFiyati" => "₺71,10",
+                ),
+                "6" => array(
+                    "resim" => "../photos/books/hikaye/yedek-kaleci.jpg",
+                    "kitapAdi" => "Yedek Kaleci",
+                    "kitapYazari" => "Hüseyin Keleş",
+                    "kitapFiyati" => "₺58,44",
+                ),
+                "7" => array(
+                    "resim" => "../photos/books/hikaye/dinozorumun-saklandigi-yer.jpg",
+                    "kitapAdi" => "Dinozorumun Saklandığı Yer",
+                    "kitapYazari" => "Mert Arık",
+                    "kitapFiyati" => "₺41,96",
+                ),
+                "8" => array(
+                    "resim" => "../photos/books/hikaye/dunyanin-en-korkak-hayvani.jpg",
+                    "kitapAdi" => "Dünyanın En Korkak Hayvanı",
+                    "kitapYazari" => "Caner Sarıoğlu",
+                    "kitapFiyati" => "₺44,00",
+                ),
+                "9" => array(
+                    "resim" => "../photos/books/hikaye/faruk.jpg",
+                    "kitapAdi" => "Çantamdan Fil Çıktı",
+                    "kitapYazari" => "Caner Sarıoğlu",
+                    "kitapFiyati" => "₺58,44",
+                ),
+        
+            );
 
-)
+            $yeniCikanlar = array(
+                "1" => array(
+                    "resim" => "../photos/books/yeni-cikanlar/sadece-Allaha-muhtacsin.jpg",
+                    "kitapAdi" => "Sadece Allah'a Muhtaçsın",
+                    "kitapYazari" => "Nilsun Urallı",
+                    "kitapFiyati" => "₺220,00",
+                ),
+                "2" => array(
+                    "resim" => "../photos/books/yeni-cikanlar/abherul-asikin.jpg",
+                    "kitapAdi" => "Abherül Aşıkın",
+                    "kitapYazari" => "Ruzbihan Bakli",
+                    "kitapFiyati" => "₺89,38",
+                ),
+                "3" => array(
+                    "resim" => "../photos/books/yeni-cikanlar/aldim-biraz-konustum.png",
+                    "kitapAdi" => "Aldım, Biraz Konuştum",
+                    "kitapYazari" => "Bayram Ali Artar",
+                    "kitapFiyati" => "₺100,00",
+                ),
+                "4" => array(
+                    "resim" => "../photos/books/yeni-cikanlar/dokuntuler.jpg",
+                    "kitapAdi" => "Döküntüler",
+                    "kitapYazari" => "Dila Cengiz",
+                    "kitapFiyati" => "₺65,00",
+                ),
+                "5" => array(
+                    "resim" => "../photos/books/yeni-cikanlar/suphe.jpg",
+                    "kitapAdi" => "Şüphe: Hayri Bey'in Vefatı İntihar mı Cinayet mi?",
+                    "kitapYazari" => "Prof. Dr. Ali Akyıldız",
+                    "kitapFiyati" => "₺178,75",
+                ),
+                "6" => array(
+                    "resim" => "../photos/books/yeni-cikanlar/altmis-oyku.jpg",
+                    "kitapAdi" => "Altmış Öykü",
+                    "kitapYazari" => "Dino Buzzati",
+                    "kitapFiyati" => "₺214,50",
+                ),
+                "7" => array(
+                    "resim" => "../photos/books/yeni-cikanlar/japon-masallari.jpg",
+                    "kitapAdi" => "Japon Masalları",
+                    "kitapYazari" => "Kunio Yanagita",
+                    "kitapFiyati" => "₺100,00",
+                ),
+                "8" => array(
+                    "resim" => "../photos/books/yeni-cikanlar/suyun-rengi.jpg",
+                    "kitapAdi" => "Suyun Rengi",
+                    "kitapYazari" => "Açelya Daştan",
+                    "kitapFiyati" => "₺100,00",
+                ),
+                "9" => array(
+                    "resim" => "../photos/books/yeni-cikanlar/aci-gecer-sizi-kalir.png",
+                    "kitapAdi" => "Acı Geçer Sızı Kalır",
+                    "kitapYazari" => "Memduh Şevket Esendal",
+                    "kitapFiyati" => "₺22,85",
+                )
+        
+                );
 
-$tarih = array(
-    "1" => array(
-        "resim" => "../photos/books/tarih/avrupa-ve-biz.jpg",
-        "kitapAdi" => "Avrupa ve Biz",
-        "kitapYazari" => "İlber Ortaylı",
-        "kitapFiyati" => "₺52,19",
-    ),
-    "2" => array(
-        "resim" => "../photos/books/tarih/cumhuriyetin-dogusu.jpg",
-        "kitapAdi" => "Cumhuriyetin Doğuşu",
-        "kitapYazari" => "İlber Ortaylı",
-        "kitapFiyati" => "₺57,10",
-    ),
-    "3" => array(
-        "resim" => "../photos/books/tarih/gazi-mustafa-kemal-atatürk.jpg",
-        "kitapAdi" => "Gazi Mustafa Kemal Atatürk",
-        "kitapYazari" => "İlber Ortaylı",
-        "kitapFiyati" => "₺52,19",
-    ),
-    "4" => array(
-        "resim" => "../photos/books/tarih/gel-dunyayi-kesfedelim.jpg",
-        "kitapAdi" => "Gel Dünyayı Keşfedelim",
-        "kitapYazari" => "İlber Ortaylı",
-        "kitapFiyati" => "₺51,56",
-    ),
-    "5" => array(
-        "resim" => "../photos/books/tarih/islam-ve-bati.jpg",
-        "kitapAdi" => "İslam ve Batı",
-        "kitapYazari" => "İbrahim Kalın",
-        "kitapFiyati" => "₺71,10",
-    ),
-    "6" => array(
-        "resim" => "../photos/books/tarih/kisa-osmanli-tarihi.jpg",
-        "kitapAdi" => "Kısa Osmanlı Tarihi",
-        "kitapYazari" => "Halil İnalcık",
-        "kitapFiyati" => "₺58,44",
-    ),
-    "7" => array(
-        "resim" => "../photos/books/tarih/milli-mucadele-tarihi.jpg",
-        "kitapAdi" => "Milli Mücadele Tarihi",
-        "kitapYazari" => "Halil İnalcık",
-        "kitapFiyati" => "₺41,96",
-    ),
-    "8" => array(
-        "resim" => "../photos/books/tarih/neden-islamin-ortacagi-yoktu.jpg",
-        "kitapAdi" => "Neden İslam'ın Ortaçağı Yoktu?",
-        "kitapYazari" => "Thomas Bauer",
-        "kitapFiyati" => "₺44,00",
-    ),
-    "9" => array(
-        "resim" => "../photos/books/tarih/teskilati-mahsusa.jpg",
-        "kitapAdi" => "Teşkilat-ı Mahsusa",
-        "kitapYazari" => "Philip H. Stoddard",
-        "kitapFiyati" => "₺58,44",
-    ),
-
+    $tarih = array(
+        "1" => array(
+            "resim" => "../photos/books/tarih/avrupa-ve-biz.jpg",
+            "kitapAdi" => "Avrupa ve Biz",
+            "kitapYazari" => "İlber Ortaylı",
+            "kitapFiyati" => "₺52,19",
+        ),
+        "2" => array(
+            "resim" => "../photos/books/tarih/cumhuriyetin-dogusu.jpg",
+            "kitapAdi" => "Cumhuriyetin Doğuşu",
+            "kitapYazari" => "İlber Ortaylı",
+            "kitapFiyati" => "₺57,10",
+        ),
+        "3" => array(
+            "resim" => "../photos/books/tarih/gazi-mustafa-kemal-atatürk.jpg",
+            "kitapAdi" => "Gazi Mustafa Kemal Atatürk",
+            "kitapYazari" => "İlber Ortaylı",
+            "kitapFiyati" => "₺52,19",
+        ),
+        "4" => array(
+            "resim" => "../photos/books/tarih/gel-dunyayi-kesfedelim.jpg",
+            "kitapAdi" => "Gel Dünyayı Keşfedelim",
+            "kitapYazari" => "İlber Ortaylı",
+            "kitapFiyati" => "₺51,56",
+        ),
+        "5" => array(
+            "resim" => "../photos/books/tarih/islam-ve-bati.jpg",
+            "kitapAdi" => "İslam ve Batı",
+            "kitapYazari" => "İbrahim Kalın",
+            "kitapFiyati" => "₺71,10",
+        ),
+        "6" => array(
+            "resim" => "../photos/books/tarih/kisa-osmanli-tarihi.jpg",
+            "kitapAdi" => "Kısa Osmanlı Tarihi",
+            "kitapYazari" => "Halil İnalcık",
+            "kitapFiyati" => "₺58,44",
+        ),
+        "7" => array(
+            "resim" => "../photos/books/tarih/milli-mucadele-tarihi.jpg",
+            "kitapAdi" => "Milli Mücadele Tarihi",
+            "kitapYazari" => "Halil İnalcık",
+            "kitapFiyati" => "₺41,96",
+        ),
+        "8" => array(
+            "resim" => "../photos/books/tarih/neden-islamin-ortacagi-yoktu.jpg",
+            "kitapAdi" => "Neden İslam'ın Ortaçağı Yoktu?",
+            "kitapYazari" => "Thomas Bauer",
+            "kitapFiyati" => "₺44,00",
+        ),
+        "9" => array(
+            "resim" => "../photos/books/tarih/teskilati-mahsusa.png",
+            "kitapAdi" => "Teşkilat-ı Mahsusa",
+            "kitapYazari" => "Philip H. Stoddard",
+            "kitapFiyati" => "₺58,44",
+        ),
+    );
 ?>

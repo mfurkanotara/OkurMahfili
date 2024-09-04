9182c52fd5dc99a19feb4dded1dc90d1507287cf
Muhammet Furkan Otara <mfurkanotara0@gmail.com>
2024-09-04 14:49:07 +0300

    variables.php dosyasına yeni arrayler eklendi.

--- libs/variables.php
+++ libs/variables.php
@@ -229,61 +229,96 @@
                 )
         
                 );
+                
 
-    $tarih = array(
-        "1" => array(
-            "resim" => "../photos/books/tarih/avrupa-ve-biz.jpg",
-            "kitapAdi" => "Avrupa ve Biz",
-            "kitapYazari" => "İlber Ortaylı",
-            "kitapFiyati" => "₺52,19",
-        ),
-        "2" => array(
-            "resim" => "../photos/books/tarih/cumhuriyetin-dogusu.jpg",
-            "kitapAdi" => "Cumhuriyetin Doğuşu",
-            "kitapYazari" => "İlber Ortaylı",
-            "kitapFiyati" => "₺57,10",
-        ),
-        "3" => array(
-            "resim" => "../photos/books/tarih/gazi-mustafa-kemal-atatürk.jpg",
-            "kitapAdi" => "Gazi Mustafa Kemal Atatürk",
-            "kitapYazari" => "İlber Ortaylı",
-            "kitapFiyati" => "₺52,19",
-        ),
-        "4" => array(
-            "resim" => "../photos/books/tarih/gel-dunyayi-kesfedelim.jpg",
-            "kitapAdi" => "Gel Dünyayı Keşfedelim",
-            "kitapYazari" => "İlber Ortaylı",
-            "kitapFiyati" => "₺51,56",
-        ),
-        "5" => array(
-            "resim" => "../photos/books/tarih/islam-ve-bati.jpg",
-            "kitapAdi" => "İslam ve Batı",
-            "kitapYazari" => "İbrahim Kalın",
-            "kitapFiyati" => "₺71,10",
-        ),
-        "6" => array(
-            "resim" => "../photos/books/tarih/kisa-osmanli-tarihi.jpg",
-            "kitapAdi" => "Kısa Osmanlı Tarihi",
-            "kitapYazari" => "Halil İnalcık",
-            "kitapFiyati" => "₺58,44",
-        ),
-        "7" => array(
-            "resim" => "../photos/books/tarih/milli-mucadele-tarihi.jpg",
-            "kitapAdi" => "Milli Mücadele Tarihi",
-            "kitapYazari" => "Halil İnalcık",
-            "kitapFiyati" => "₺41,96",
-        ),
-        "8" => array(
-            "resim" => "../photos/books/tarih/neden-islamin-ortacagi-yoktu.jpg",
-            "kitapAdi" => "Neden İslam'ın Ortaçağı Yoktu?",
-            "kitapYazari" => "Thomas Bauer",
-            "kitapFiyati" => "₺44,00",
-        ),
-        "9" => array(
-            "resim" => "../photos/books/tarih/teskilati-mahsusa.png",
-            "kitapAdi" => "Teşkilat-ı Mahsusa",
-            "kitapYazari" => "Philip H. Stoddard",
-            "kitapFiyati" => "₺58,44",
-        ),
-    );
+            $oneCikanKitaplar = array(
+                "1" => array(
+                    "kitapAdi" => "Ahirete Yolculuk",
+                    "aciklama" => "Bu açıklama, Ahirete Yolculuk kitabını açıklıyor.",
+                    "resim" => "../photos/books/index-book-img/ahirete-yolculuk.jpg",
+                ),
+                "2" => array(
+                    "kitapAdi" => "Bir Başka Din Tasavvuf",
+                    "aciklama" => "Bu açıklama, Bir Başka Din Tasavvuf kitabını açıklıyor.",
+                    "resim" => "../photos/books/index-book-img/bir-baska-din-tasavvuf.jpg",
+                ),
+                "3" => array(
+                    "kitapAdi" => "Feyzü'l-Furkan",
+                    "aciklama" => "Bu açıklama, Feyzü'l-Furkan kitabını açıklıyor.",
+                    "resim" => "../photos/books/index-book-img/feyzul-furkan.jpg",
+                ),
+                "4" => array(
+                    "kitapAdi" => "Haber Delili",
+                    "aciklama" => "Bu açıklama, Haber Delili kitabını açıklıyor.",
+                    "resim" => "../photos/books/index-book-img/haber-delili.jpg",
+                ),
+                "5" => array(
+                    "kitapAdi" => "İslam ve Batı",
+                    "aciklama" => "Bu açıklama, İslam ve Batı kitabını açıklıyor.",
+                    "resim" => "../photos/books/index-book-img/islam-ve-bati.jpg",
+                ),
+                "6" => array(
+                    "kitapAdi" => "Muhtelif 1",
+                    "aciklama" => "Bu açıklama, Muhtelif 1 kitabını açıklıyor.",
+                    "resim" => "../photos/books/index-book-img/muhtelif-1.jpg",
+                ),
+
+            );
+
+                $tarih = array(
+                    "1" => array(
+                        "resim" => "../photos/books/tarih/avrupa-ve-biz.jpg",
+                        "kitapAdi" => "Avrupa ve Biz",
+                        "kitapYazari" => "İlber Ortaylı",
+                        "kitapFiyati" => "₺52,19",
+                    ),
+                    "2" => array(
+                        "resim" => "../photos/books/tarih/cumhuriyetin-dogusu.jpg",
+                        "kitapAdi" => "Cumhuriyetin Doğuşu",
+                        "kitapYazari" => "İlber Ortaylı",
+                        "kitapFiyati" => "₺57,10",
+                    ),
+                    "3" => array(
+                        "resim" => "../photos/books/tarih/gazi-mustafa-kemal-atatürk.jpg",
+                        "kitapAdi" => "Gazi Mustafa Kemal Atatürk",
+                        "kitapYazari" => "İlber Ortaylı",
+                        "kitapFiyati" => "₺52,19",
+                    ),
+                    "4" => array(
+                        "resim" => "../photos/books/tarih/gel-dunyayi-kesfedelim.jpg",
+                        "kitapAdi" => "Gel Dünyayı Keşfedelim",
+                        "kitapYazari" => "İlber Ortaylı",
+                        "kitapFiyati" => "₺51,56",
+                    ),
+                    "5" => array(
+                        "resim" => "../photos/books/tarih/islam-ve-bati.jpg",
+                        "kitapAdi" => "İslam ve Batı",
+                        "kitapYazari" => "İbrahim Kalın",
+                        "kitapFiyati" => "₺71,10",
+                    ),
+                    "6" => array(
+                        "resim" => "../photos/books/tarih/kisa-osmanli-tarihi.jpg",
+                        "kitapAdi" => "Kısa Osmanlı Tarihi",
+                        "kitapYazari" => "Halil İnalcık",
+                        "kitapFiyati" => "₺58,44",
+                    ),
+                    "7" => array(
+                        "resim" => "../photos/books/tarih/milli-mucadele-tarihi.jpg",
+                        "kitapAdi" => "Milli Mücadele Tarihi",
+                        "kitapYazari" => "Halil İnalcık",
+                        "kitapFiyati" => "₺41,96",
+                    ),
+                    "8" => array(
+                        "resim" => "../photos/books/tarih/neden-islamin-ortacagi-yoktu.jpg",
+                        "kitapAdi" => "Neden İslam'ın Ortaçağı Yoktu?",
+                        "kitapYazari" => "Thomas Bauer",
+                        "kitapFiyati" => "₺44,00",
+                    ),
+                    "9" => array(
+                        "resim" => "../photos/books/tarih/teskilati-mahsusa.png",
+                        "kitapAdi" => "Teşkilat-ı Mahsusa",
+                        "kitapYazari" => "Philip H. Stoddard",
+                        "kitapFiyati" => "₺58,44",
+                    ),
+                );
 ?>

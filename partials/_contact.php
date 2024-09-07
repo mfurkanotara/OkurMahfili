09d5c3331dfd9f9787ae684f6d787831658be0a3
Muhammet Furkan Otara <mfurkanotara0@gmail.com>
2024-09-08 00:23:31 +0300

    form gönderme metodu 'post' dolarak değiştirildi

--- partials/_contact.php
+++ partials/_contact.php
@@ -27,7 +27,7 @@
     <div class="container">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12388.825146898573!2d37.199873270207824!3d39.07899870415569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40785460c4cb00df%3A0xa9710d9e56ef5dba!2zTWFuY8SxbMSxaywgNTg5MDIgTWFuY8SxbMSxay9LYW5nYWwvU2l2YXM!5e0!3m2!1str!2str!4v1722690563183!5m2!1str!2str" class="map" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-        <form class="form">
+        <form action="index.php" class="form" method="post">
             <div class="form-group">
                 <label for="fname">Adı</label>
                 <input type="text" class="form-control" id="fname" placeholder="Adınızı giriniz.">
